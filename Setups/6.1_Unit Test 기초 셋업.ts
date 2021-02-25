e91f0f343caf8a2004077a71ec5f6faee453b778
Bugod96 <bugod96@gmail.com>
2021-02-26 01:58:18 +0900

    Jest Setup

--- Setups/6.1_Unit Test 기초 셋업.ts
+++ Setups/6.1_Unit Test 기초 셋업.ts
@@ -5,20 +5,43 @@ Unit Test : Service의 개별 메서드에 대한 테스트. 최대한 구분하
 
 1) 테스트할 서비스(UsersService)로 테스트 모듈을 따로 생성하여 테스트 실행.  
    : beforeEach로 각 테스트 전에 테스트 모듈 생성하여 컴파일하도록.
-   : 비교) beforeAll 사용시, toHaveBeenCalledTimes(1); 테스트 어려워짐. 누적 카운트됨.
+   : 비교) beforeAll 사용시, toHaveBeenCalledTimes 테스트 어려워짐. 누적 카운트됨.
 2) 생성되는 테스트 모듈의 서비스 사용할 수 있도록 let 변수에 대입.
 */
 import { Test } from "@nestjs/testing";
-import { UsersService } from "./4.1_users.service";
+import { UsersService } from "./users.service";
+
+const mockRepository = () => ({
+  findOne: jest.fn(), // 테스트하는 서비스에서 사용되는 모든 Repo 메서드들 설정.
+  findOneOrFail: jest.fn(),
+  save: jest.fn(), // 실제로 DB에 SQL문 보내지 않음.
+  create: jest.fn(),
+  delete: jest.fn(),
+}); // 중요: mockRepository가 객체인 경우 verificationRepository.create & usersRepository.create 메서드는 서로 동일한 것으로 간주됨.
+// 객체를 반환하는 함수로 설정하여 실행해줘야 verificationRepository.create & usersRepository.create 메서드가 서로 다른 것으로 간주됨.
+
+const mockJwtService = () => ({
+  sign: jest.fn(() => "signed-token"), // mock function으로서 세부 기능 지정. (signed-token라는 문자열을 반환)
+  verify: jest.fn(), // JwtService에서 사용되는 모든 메서드들 설정.
+}); // 객체를 반환하는 함수로 설정하여 실행해줘야 테스트 누적 카운트 문제 해결.
 
 describe("UserService", () => {
   let service: UsersService; // 2) 테스트 모듈의 서비스 접근 목적
+  let usersRepository: MockRepository<UserEntity>; // 4) MockRepo 사용 목적. TS 기능
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [UsersService], // 모듈에서 사용할 서비스 설정.
+      providers: [
+        UsersService, // 모듈에서 사용할 서비스 설정.
+        // 3) Mocking UserEntity. TypeORM 속이기. (provide & useValue)
+        { provide: getRepositoryToken(UserEntity), useValue: mockRepository() },
+        { provide: JwtService, useValue: mockJwtService() }, // constructor의 외부 Entity, 서비스들 faking
+      ],
     }).compile(); // 1) 각 테스트 전에 테스트 모듈 생성하여 컴파일하도록.
     service = module.get<UsersService>(UsersService); // 2) 테스트 모듈의 서비스 사용할 수 있도록 let 변수에 대입.
+    usersRepository = module.get(getRepositoryToken(UserEntity));
+    jwtService = module.get<JwtService>(JwtService);
   });
 
   it("should be defined", () => {
--- Setups/6.2_E2E Test 기초 셋업.ts
+++ Setups/6.2_E2E Test 기초 셋업.ts
@@ -1,17 +1,40 @@
 /*
-  AppModule로 테스트 모듈 생성하여 실행하고 멈춰줘야 함.
+  AppModule로 테스트 앱모듈 생성하여 실행하고 멈춰줘야 함.
   - beforeAll - app.init();
   - afterAll - app.close();
+
+  테스트 앱모듈 생성 후 Entity로 Repository 사용 가능.
+  - let usersRepo: Repository<UserEntity>;
+  - usersRepo = module.get(getRepositoryToken(UserEntity)
 
   테스트 시작시 DB migration 진행. 사용자 생성 등 작업 실제로 발생.
   => 테스트 종료시 test DB 지우기. drop database. - 별도 셋업 필요.
 */
+/*
+  개별 resolver 테스트 방법
+  - return request(app.getHttpServer()).post('/graphql').send({데이터}).expect(~).expect(~)
+    - request 결과 반환 형식. ( == supertest) // import * as request from 'supertest';
+    - app.getHttpServer로 요청 보내기.
+    - .post: GrapqhQL Endpoint(localhost:3000/)로 post 요청 보내기.
+           : 참고) GraphQL의 본질은 post request  
+    - .send: post 요청으로 데이터 보내기. playground에서의 query/mutation 형식.
+           : 데이터는 `` 템플릿 리터럴 활용.
+           : 참고) ""도 가능은 함. but it's ugly. {query: "{loggedInUser{id}}"}
+    - .expect(~): 해당 요청 결과에 따른 결과 예상.       
+                : statuscode 예상 가능.
+                : 요청결과 콜백함수의 매개변수로 받아 활용 가능.
+
+  => resolver & TypeORM & Service 메서드 모두 제대로 동작하는지 동시에 확인 가능.
+*/
 import { Test, TestingModule } from "@nestjs/testing";
+import * as request from "supertest";
 import { getConnection } from "typeorm";
+import { INestApplication } from "@nestjs/common";
 import { AppModule } from "../src/app.module";
 
 describe("UsersModule (e2e)", () => {
-  let app;
+  let app: INestApplication;
+  let usersRepo: Repository<UserEntity>;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -19,14 +42,42 @@ describe("UsersModule (e2e)", () => {
     }).compile();
     app = module.createNestApplication();
     await app.init();
+    usersRepo = module.get<Repository<UserEntity>>(
+      getRepositoryToken(UserEntity)
+    );
   });
 
   afterAll(async () => {
-    getConnection().dropDatabase(); // typeOrm. 테스트 종료시 drop database
+    await getConnection().dropDatabase(); // typeOrm. 테스트 종료시 drop database
     app.close();
   });
 
-  it.todo("createAccount");
+  describe('createAccount', () => {
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post('/graphql')
+        .send({
+          query: `
+            mutation {
+              createAccount(input: {
+                email:"bugod96@gmail.com", 
+                password:"testtest",  
+                role: Delivery
+              }){
+                ok,
+                error 
+              }
+          }
+          `,
+        })
+        .expect(200) // 요청결과 status code 200 예상.
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+          console.log(res.body);
+        }); // 콜백함수로 요청결과 담김. 요청결과로 반환된 값 확인 가능.
+    });
+
   it.todo("login");
   it.todo("loggedInUser");
   it.todo("userProfile");
